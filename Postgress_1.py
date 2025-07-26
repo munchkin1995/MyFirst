4b2de73b2125838ab2912f252af83281c633b7c0
payal <pylthakur95@gmail.com>
2025-07-26 16:15:34 +0530

    my first commit

---
+++ Postgress_1.py
@@ -0,0 +1,26 @@
+from optparse import Values
+
+import psycopg2
+import pandas as pd
+
+# Define connection parameters
+conn = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    database="test",
+    user="postgres",
+    password="Root"
+)
+
+
+query = "select count(*) from person"
+
+df = pd.read_sql(query, conn)
+
+# df['country'] = 'india'
+print((df["count"][0]))
+
+
+# Close the connection
+# cursor.close()
+conn.close()
